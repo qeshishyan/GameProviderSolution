166817cc02d482b292d42a67744688ce85de00c9
keshishyan <keshishyan1997@gmail.com>
2023-09-02 15:16:29 +0400

    added DI

---
+++ src/UserService/Extensions/DependencyInjectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using UserService.Repositories;
+using UserService.Services;
+
+namespace UserService.Extensions
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, Services.UserService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}
